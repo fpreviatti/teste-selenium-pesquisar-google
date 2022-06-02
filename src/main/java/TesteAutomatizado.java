6d5fd0488ce8057636399f8511ae069e31c7f83a
Fabio Previatti <fpreviatti@hotmail.com>
2022-06-02 18:02:21 -0300

    ajustados comentarios

--- src/main/java/TesteAutomatizado.java
+++ src/main/java/TesteAutomatizado.java
@@ -2,12 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteAutomatizado {
 
     public static void main(String[] args) {
-// abre firefox
+//abre o chrome, necessario utilizar o driver compativel com a versao do navegador
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\sacarolhas\\Documents\\teste-selenium\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 // acessa o site do google
